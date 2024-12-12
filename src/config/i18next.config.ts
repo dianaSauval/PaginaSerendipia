1996d42b21164b76c4460373d462f769c7ee1971
dianasauval <dianasauval@hotmail.com>
2024-12-12 11:43:07 -0300

    add style, favicon, etc

--- src/config/i18next.config.ts
+++ src/config/i18next.config.ts
@@ -1,9 +1,23 @@
 import i18next from "i18next";
-import Backend from "i18next-http-backend"
+import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
+i18next
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: "es",
+    fallbackLng: "es", // Idioma predeterminado
+    supportedLngs: ["en", "es"], // Idiomas soportados
+    backend: {
+      // Ruta relativa donde están los archivos de traducción
+      loadPath: '/locales/{{lng}}/global.json',
+    },
+    interpolation: {
+      escapeValue: false, // React ya maneja el escape
+    },
+    debug: false, // Activa logs para depurar
+  });
 
-i18next.use(Backend).use(initReactI18next).init({
-    fallbackLng: "es",
-}  
-)
+export default i18next;
+
