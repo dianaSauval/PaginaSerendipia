ed1bf8977156e57fff6423eb1f0e3165ccc073c9
dianasauval <dianasauval@hotmail.com>
2024-12-29 18:44:42 -0300

    add function email and whatsapp

--- src/components/contact/contactSection.tsx
+++ src/components/contact/contactSection.tsx
@@ -19,20 +19,21 @@ export default function ContactSection() {
         <div className="info">
           <h2>{t("contactSection.recruitment")}</h2>
           <div className="mail">
-            <MailOutlineIcon sx={{ fontSize: 40 }} />
-            <p>dianasauval@hotmail.com</p>
+            <MailOutlineIcon sx={{ fontSize: 40 }} className="mail-icon" />
+            <a href="mailto:dianasauval@hotmail.com" className="email-link">
+              dianasauval@hotmail.com
+            </a>
           </div>
           <div className="mail whatsapp">
-  <NavLink
-    to="https://wa.me/5491159392983"
-    target="_blank"
-    className="whatsapp-link"
-  >
-    <WhatsAppIcon sx={{ fontSize: 40 }} className="whatsapp-icon" />
-    <p className="whatsapp-number">+54 9 11 5939-2983</p>
-  </NavLink>
-</div>
-
+            <NavLink
+              to="https://wa.me/5491159392983"
+              target="_blank"
+              className="whatsapp-link"
+            >
+              <WhatsAppIcon sx={{ fontSize: 40 }} className="whatsapp-icon" />
+              <p className="whatsapp-number">+54 9 11 5939-2983</p>
+            </NavLink>
+          </div>
         </div>
         <div className="networks">
           <ul>
@@ -40,17 +41,17 @@ export default function ContactSection() {
               <NavLink
                 to="https://www.tiktok.com/@duo.serendipia"
                 target="_blank"
-                style={{ color: '#262626' }}
+                style={{ color: "#262626" }}
               >
                 <div className="icon">
                   <svg
-                  className="tiktok-icon"
+                    className="tiktok-icon"
                     xmlns="http://www.w3.org/2000/svg"
                     width="100%"
                     height="100%"
                     viewBox="0 0 25 50"
                     fill="currentColor"
-                    id="tiktok"             
+                    id="tiktok"
                   >
                     <path d="M 6 3 C 4.3550302 3 3 4.3550302 3 6 L 3 18 C 3 19.64497 4.3550302 21 6 21 L 18 21 C 19.64497 21 21 19.64497 21 18 L 21 6 C 21 4.3550302 19.64497 3 18 3 L 6 3 z M 6 5 L 18 5 C 18.56503 5 19 5.4349698 19 6 L 19 18 C 19 18.56503 18.56503 19 18 19 L 6 19 C 5.4349698 19 5 18.56503 5 18 L 5 6 C 5 5.4349698 5.4349698 5 6 5 z M 12 7 L 12 14 C 12 14.56503 11.56503 15 11 15 C 10.43497 15 10 14.56503 10 14 C 10 13.43497 10.43497 13 11 13 L 11 11 C 9.3550302 11 8 12.35503 8 14 C 8 15.64497 9.3550302 17 11 17 C 12.64497 17 14 15.64497 14 14 L 14 10.232422 C 14.616148 10.671342 15.259118 11 16 11 L 16 9 C 15.952667 9 15.262674 8.7809373 14.78125 8.3613281 C 14.299826 7.941719 14 7.4149911 14 7 L 12 7 z"></path>
                   </svg>
