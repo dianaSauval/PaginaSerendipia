d8632e5ed16917156d2d6767ae3873f31c41372f
dianasauval <dianasauval@hotmail.com>
2025-02-07 16:44:48 -0300

    Solucionando errores de credenciales

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ import Footer from "../components/footer/Footer";
 function Home() {
   return (
     <div>        
-      <header>
+    <header>
         <Header />
         <Banner/>
       </header>
