1cc15750c661467f11430558c9f0692c8bed246b
Diana Sauval <dianasauval@hotmail.com>
2026-05-21 16:15:39 +0200

    agregando pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 import { Suspense } from "react";
 import CircularIndeterminate from "./components/spinner/Spinner";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/theme";
+import ShowSpinology from "./pages/ShowSpinology";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Suspense fallback={<CircularIndeterminate/>}>
-    <div className="App">
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </HashRouter>
-    </div>
-    </Suspense>
+      <Suspense fallback={<CircularIndeterminate />}>
+        <div className="App">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/show-spinology" element={<ShowSpinology />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </Suspense>
     </ThemeProvider>
   );
 }
---
+++ src/pages/ShowSpinology.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ShowSpinology() {
+  return (
+    <main>
+      <h1>Spinology</h1>
+      <p>Página para propinas y contrataciones</p>
+    </main>
+  );
+}
+
+export default ShowSpinology;
